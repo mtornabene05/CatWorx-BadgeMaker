e3f0048969e27a98a8cd3fa42b174517724b6273
Maria Tornabene <tornabenem@zin-tech.com>
2022-06-10 15:09:09 -0400

    end of lesson 2.3

--- Program.cs
+++ Program.cs
@@ -7,13 +7,22 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-            List<string> employees = new List<string>() { "adam", "amy" };
-            employees.Add("barbara");
-            employees.Add("billy");
-            Console.WriteLine("Please enter a name: ");
-            //Get a name from the console and assign it to a variable
-            string input = Console.ReadLine();
-            employees.Add(input);
+            List<string> employees = new List<string>();
+            
+            //Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit) ");
+                //Get a name from the console and assign it to a variable
+                string input = Console.ReadLine();
+                //Break if the user hits ENTER without typing a name
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+
             for (int i = 0; i< employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
