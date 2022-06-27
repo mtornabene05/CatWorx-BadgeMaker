5ee6f03d7f6284fdf51f3457194513cf1ec42c9c
Maria Tornabene <tornabenem@zin-tech.com>
2022-06-27 11:35:00 -0400

    added ability to collect and print all employee property values

--- Program.cs
+++ Program.cs
@@ -5,39 +5,50 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
             //I will return a list of strings
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             //Collect user values until the value is an empty string
             while (true)
             {
                 Console.WriteLine("Please enter a name: (leave empty to exit) ");
                 //Get a name from the console and assign it to a variable
-                string input = Console.ReadLine();
+                string firstName = Console.ReadLine();
                 //Break if the user hits ENTER without typing a name
-                if (input == "")
+                if (firstName == "")
                 {
                     break;
                 }
-                employees.Add(input);
+
+                //add a Console.ReadLine() for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine();
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
             }
 
             return employees;
         }
 
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
             }
         }
 
         static void Main(string[] args)
         {
-            List<string> employees = GetEmployees();            
+            List<Employee> employees = GetEmployees();        
             PrintEmployees(employees);
+         
         }
     }
 }
